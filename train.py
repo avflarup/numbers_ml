7da08911513210d8e7ad0becb74a974fbe823b64
au681379 <andreas@vaagholt.dk>
2026-04-30 19:35:11 +0200

    Added structure to project

--- train.py
+++ train.py
@@ -2,44 +2,30 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Compose, Normalize
+from models.model import NeuralNetwork
 
-training_data = datasets.MNIST( 
+transform = Compose([
+    ToTensor(),
+    Normalize((0.1307,), (0.3081,))
+])
+
+training_data = datasets.MNIST(
     root="data",
     train=True,
     download=True,
-    transform=ToTensor(),
+    transform=transform,
 )
 
 test_data = datasets.MNIST(
     root="data",
     train=False,
     download=True,
-    transform=ToTensor(),
+    transform=transform,
 )
 
-batch_size = 64
-#create data loaders
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-#define model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
-
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64)
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -72,15 +58,18 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-
 model = NeuralNetwork()
+
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 epochs = 5
+
 for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+    print(f"Epoch {t+1}")
+    train(train_dataloader, model, loss_fn, optimizer, device)
+    test(test_dataloader, model, loss_fn, device)
+
+torch.save(model.state_dict(), "models/mnist_model.pth")
 print("Done!")
 
